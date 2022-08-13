7a07f081918acbf17775114a51e9471a5c684668
Amit Ghosh <110232309+amitghosheda@users.noreply.github.com>
2022-08-14 00:54:36 +0530

    Update switch.sh

--- switch.sh
+++ switch.sh
@@ -7,13 +7,13 @@ echo -e "Enter '1' Addition \nEnter '2' Substraction \nEnter '3' multiplication 
 echo "Enter your choice :"
 read i
 case $i in
-1)echo "$scale=3; first + $second" | bc ;;
+1)echo "$scale=2; first + $second" | bc ;;
 
-2)echo "scale=3; $first - $second" | bc ;;
+2)echo "scale=2; $first - $second" | bc ;;
 
-3)echo "scale=3; $first * $second" | bc ;;
+3)echo "scale=2; $first * $second" | bc ;;
 
-4)echo "scale=3; $first / $second" | bc ;;
+4)echo "scale=2; $first / $second" | bc ;;
 
 *)echo "You gave a Wrong input" ;;
 esac
