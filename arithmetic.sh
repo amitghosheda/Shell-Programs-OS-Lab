2f29ba36287432b0d33d34bcea91d502a9d02a10
Amit Ghosh <110232309+amitghosheda@users.noreply.github.com>
2022-08-14 12:00:44 +0530

    Update arithmetic.sh

--- arithmetic.sh
+++ arithmetic.sh
@@ -3,11 +3,11 @@ read first
 echo "Enter Second number:"
 read second
 echo "Enter the Following Option:"
-echo -e "Enter '1' Addition \nEnter '2' Substraction \nEnter '3' multiplication \nEnter '4' Divison :"
+echo "Enter '1' Addition \nEnter '2' Substraction \nEnter '3' multiplication \nEnter '4' Divison :"
 echo "Enter your choice :"
 read i
 case $i in
-1)echo "$scale=2; first + $second" | bc ;;
+1)echo "scale=2; $first + $second" | bc ;;
 
 2)echo "scale=2; $first - $second" | bc ;;
 
